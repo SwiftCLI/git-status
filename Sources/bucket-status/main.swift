0a7351c36703583a9f4223dad816c9009988dbdf
steve_dao <steve_dao@ntucenterprise.sg>
2021-06-08 00:05:33 +0800

    added parser

--- Sources/bucket-status/main.swift
+++ Sources/bucket-status/main.swift
@@ -1,1 +1,9 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct BucketStatus: ParsableCommand {
+    mutating func run() throws {
+        print("hello world")
+    }
+}
+
+BucketStatus.main()
